10c9a64c70b4fcfdb799b367e8e150c249a4db2e
Amanda <philhomic@qq.com>
2017-03-12 18:03:37 +0800

    Update task1.js

--- nuomi/phantomjs/task1/task1.js
+++ nuomi/phantomjs/task1/task1.js
@@ -1,56 +1,61 @@
-var page = require("webpage").create(); 
-var system = require("system"); 
-
-if(system.args.length === 1){
-    console.log("Usage: phantomjs task4.js <some keyword>"); 
-    phantom.exit(); 
-}
- 
+var page = require('webpage').create();
+var system = require('system');
+var url, keyword;
 var result = {
-       code: 0, 
-       msg: "",
-       word: system.args[1],
-       time: Date.now(), 
-       dataList: []
+	code: 0,
+	msg: '抓取失败',
+	word: '',
+	time: 'N/A',
+	dataList: []
+};
+var t = Date.now();
+
+if(system.args.length == 1){
+	console.log('Usage: phantomjs test.js <some keyword>');
+	phantom.exit();
 }
 
-
-var url = "https://baidu.com/s?wd=" + result.word; 
+keyword = system.args[1];
+result.word = keyword;
+url = 'http://www.baidu.com/s?wd=' + keyword;
 
 page.open(url, function(status){
-    if(status !== "success"){
-        result.msg = "capture failed"; 
-        result.time = -1; 
-        console.log(JSON.stringify(result)); 
-        phantom.exit(); 
+	if(status !== 'success'){
+		return result;
+	} else {
+		result.code = 1;
+		result.msg = '抓取成功';
 
-    }
-    else{
-        
-        result.time = Date.now() - result.time; 
-        result.dataList = page.evaluate(function(){
-            var containers = document.getElementsByClassName("c-container"); 
-            var dataList = new Array(); 
-            for(var i = 0; i < containers.length; i++){
-                var ele = containers[i];  
-                var link = ele.getElementsByClassName("t")[0].getElementsByTagName("a")[0]; 
-                var abstract = ele.getElementsByClassName("c-abstract")[0] || new Object(); 
-                var pic = ele.getElementsByClassName("c-img")[0]; 
-                if(pic){
-                    pic = pic.getAttribute("src"); 
-                }
-                dataList.push({
-                    title: link.innerText || "", 
-                    info: abstract.innerText || "",
-                    link: link.getAttribute("href") || "", 
-                    pic: pic || ""
-                });
-            } 
-            return dataList; 
-        }); 
-        result.code = 1; 
-        result.msg = "capture success"; 
-        console.log(JSON.stringify(result)); 
-        phantom.exit();  
-        }
-}); 
+		page.includeJs('https://code.jquery.com/jquery-2.2.4.min.js', function(){
+			var info, length, i;
+
+				length = page.evaluate(function(){
+					return $('div.c-container').length;
+				});
+
+				for(i = 1; i <= length; i++){
+					info = page.evaluate(function(index){
+						var title = $('#' + index + '>h3').text();
+						var link = $('#' + index + '>h3>a').attr('href');
+						var abstract = $('#' + index + '>.c-abstract').text() || '';
+						var pic = $('#' + index + ' .c-img').attr('src') || '';
+						return {
+							title: title,
+							info: abstract,
+							link: link,
+							pic: pic
+						}
+					}, i);
+
+					result.dataList.push(info);	
+				}
+
+				t = Date.now() - t;
+				result.time = t;
+
+				console.log(JSON.stringify(result));
+				phantom.exit();
+				
+		})
+	}
+})
